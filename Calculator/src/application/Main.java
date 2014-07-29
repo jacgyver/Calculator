4928b458c86aee22545dc6f421aaf8fbfaa86399
Administrator <Administrator@2-21>
2014-07-29 17:45:35 +0900

    commit-14-07-29

--- Calculator/src/application/Main.java
+++ Calculator/src/application/Main.java
@@ -1,26 +1,58 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	private Scene scene;
+	private BorderPane root;
+	private Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
+			this.primaryStage = primaryStage;
+			this.primaryStage.getIcons().add(new Image("file:resources/calculator.jpg"));
+			this.primaryStage.setTitle("Calculator for JAVA");
+			
+			
+
+			initLayout();
+			calculatorLayout();
+			
+	}
+	private void initLayout() {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("RootLayout.fxml"));
-			Scene scene = new Scene(root,400,400);
+			root = (BorderPane)FXMLLoader.load(getClass().getResource("view/RootLayout.fxml"));
+			scene = new Scene(root,700,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	private void calculatorLayout() {
+		try {
+			AnchorPane calculator = (AnchorPane)FXMLLoader.load(getClass().getResource("view/CalculatorLayout.fxml"));
+			root.setCenter(calculator);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ Calculator/src/application/view/RootController.java
@@ -0,0 +1,11 @@
+package application.view;
+
+import javafx.fxml.FXML;
+
+public class RootController {
+	@FXML
+	public void handleClose() {
+		System.exit(0);
+	}
+	
+}
